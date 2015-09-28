6a08ab391c319de8b2860fc78a02a9130a00493e
Vladimir Lesnykh <v.a.lesnykh@gmail.com>
2015-09-28 17:58:23 +0400

    Implements test examples for the class Array

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
+/*
+ * Author: Vladimir
+ *
+ */
+
 #include <iostream>
 
-using namespace std;
+#include "Array.h"
+#include "IOArray.h"
 
-int main() {
-    cout << "Hello, World!" << endl;
+
+int main()
+{
+	//<	A
+	Array a(12);
+	scanArray(a, "A");
+	printArray(a, "Array A");
+
+	//< B
+	Array b(3);
+	std::cout << std::endl;
+	std::cout << "Init B" << std::endl;
+	std::cout << b[0] << "; " << b[1] << "; " << b[2] << std::endl;
+
+	b = a;
+	b.sort();
+	printArray(b, "Array B");
+
+	//< C
+	Array c(25);
+	scanArrayRand(c, "C");
+	printArray(c, "Array C");
+
     return 0;
 }
